9d5d83f16c88a3f28b93c5056bec4572af8bb793
Sam Powers <sampowers@gmail.com>
2013-09-21 19:53:47 -0700

    mac stuff

--- macbook.sh
+++ macbook.sh
@@ -10,3 +10,7 @@ opts="$opts FingerLow=26 FingerHigh=32 PalmDetect=on"
 opts="$opts VertTwoFingerScroll=1 HorizTwoFingerScroll=1"
 
 for x in $opts; do synclient $x ; done
+
+
+echo 12000 | sudo tee /sys/class/backlight/gmux_backlight/brightness
+echo 50 | sudo tee /sys/class/leds/smc::kbd_backlight/brightness
